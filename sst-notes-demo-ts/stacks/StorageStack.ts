b14d84fd9d01cc4fd8c223c99fe2f907e49f9a85
Alexander Zinn <azinn@goodleap.com>
2021-11-16 08:57:28 -0500

    Enabling CORS

--- sst-notes-demo-ts/stacks/StorageStack.ts
+++ sst-notes-demo-ts/stacks/StorageStack.ts
@@ -1,15 +1,16 @@
-import * as sst from '@serverless-stack/resources';
+import * as sst from '@serverless-stack/resources'
+import { HttpMethods } from '@aws-cdk/aws-s3'
 
 export interface StorageStackProps extends sst.StackProps {
-  STAGE: string;
-  bucketName: string;
+  STAGE: string
+  bucketName: string
 }
 export default class StorageStack extends sst.Stack {
-  public table;
-  public bucket;
+  public table
+  public bucket
 
   constructor(scope: sst.App, id: string, props: StorageStackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // DynamoDB Table
     this.table = new sst.Table(this, 'notes', {
@@ -21,12 +22,20 @@ export default class StorageStack extends sst.Stack {
         noteId: sst.TableFieldType.STRING
       },
       primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' }
-    });
+    })
 
     this.bucket = new sst.Bucket(this, 'uploads', {
       s3Bucket: {
-        bucketName: props.bucketName
+        bucketName: props.bucketName,
+        cors: [
+          {
+            maxAge: 3000,
+            allowedOrigins: ['*'],
+            allowedHeaders: ['*'],
+            allowedMethods: [HttpMethods.GET, HttpMethods.PUT, HttpMethods.POST, HttpMethods.DELETE, HttpMethods.HEAD]
+          }
+        ]
       }
-    });
+    })
   }
 }
